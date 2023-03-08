c70a32454c895a251925b207f27844de6ccb045d
EricSiahaan <eric.hansdeka78@gmail.com>
2023-03-08 16:52:27 +0700

    Javascript

---
+++ introduce-javascript/script.js
@@ -0,0 +1,143 @@
+/* 1. Variable */
+
+//let myFirstVariable = 250
+// const myFirstVariable = 250
+
+// console.log(myFirstVariable)
+
+// myFirstVariable = 251
+
+// console.log(myFirstVariable)
+
+
+/* 2. Data type */
+
+// const myAge = 28
+// const amIAbove18 = true
+// const myName = "mehul"
+// const friends = [22, true, "mehul"]
+// const someSpecialValue = undefined
+
+// console.log(friends);
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(someSpecialValue);
+
+/* 3. Type of */
+
+// const myAge = 28
+// const typeofMyAge = (typeof myAge)
+// //const typeofMyAge = typeof myAge
+
+
+// console.log(typeofMyAge);
+
+/* 4.Language Fundamental */
+
+// const myAge = 22
+
+// // myAge + 1 => wrong
+// const myUpdateAge = myAge + 1
+
+// console.log(myUpdateAge)
+
+// const friendsAge = [20, 25, 20, 25, 20]
+// const sum = friendsAge[0] + friendsAge[1] + friendsAge[2] + friendsAge[3] + friendsAge[4]
+// console.log("Rata-rata usia teman", sum)
+
+// const totalFriend = 5
+// console.log("Total Teman", totalFriend)
+
+// const average = sum / totalFriend
+// console.log("Rata rata Usia teman saya", average)
+
+/* 5.Increament Decreament */
+
+// let myAge = 22
+// myAge++
+// console.log(myAge);
+// myAge = myAge + 2
+// // console.log(myAge);
+// console.log(myAge);
+
+/* 6.Function */
+
+// let myAge = 22
+// let sum = 200
+// let totalCount = 5
+
+// // function makeConsoleLog() {
+// //     console.log("This a function test")
+// //     console.log(totalCount)
+// // }
+
+// // makeConsoleLog()
+
+// function printAllVariable() {
+//     console.log('myAge', myAge)
+//     console.log('mySum', sum)
+//     console.log('totalcount', totalCount)
+// }
+
+// printAllVariable()
+
+// myAge = 28
+
+// printAllVariable()
+
+
+/* 7.Function Parameters */
+
+// let myAge = 22
+// let sum = 200
+// let totalCount = 5
+
+// function addsToMyAge(IncreamentBy, multiplyBy) {
+//     myAge = (myAge + IncreamentBy) * multiplyBy
+// }
+
+// addsToMyAge(5, 2)
+// console.log(myAge);
+
+/* 8.Return Statement */
+
+// let myAge = 10
+// let sum = 200
+// let totalCount = 5
+
+// // function addsToMyAge(IncreamentBy, multiplyBy) {
+// //     return (myAge + IncreamentBy) * multiplyBy
+// // }
+
+// // const myNewAge = addsToMyAge(2, 2)
+// //console.log(myAge, myNewAge)
+
+// function addsToMyAge(increamentBy, multiplyBy) {
+//     return sum = sum + 100
+//     // return
+
+// }
+
+// const myNewAge = addsToMyAge(5, 2)
+// console.log(sum, totalCount)
+
+/* 9.If - Else Condition */
+
+let myAge = 10
+let sum = 200
+let totalCount = 5
+
+function showIf(age) {
+    if (age > 18) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// showIf(20)
+
+console.log(showIf(20))
+// console.log(showIf(20))
+// console.log(showIf(12))
